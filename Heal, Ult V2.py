78c056ad729ed31c8dec9e40c3e94f77ce30631e
DictatorKev <103073906+DictatorKev@users.noreply.github.com>
2022-06-26 18:26:48 +0200

    Strong-Menu
    Menu has been changed, so that it can be used combined with every game there is.

--- Heal, Ult V2.py
+++ Heal, Ult V2.py
@@ -6,32 +6,37 @@ import time
 import keyboard
 import os
 from tkinter import*
+import pyperclip
+
 
 
 def new_window():
     master1 = tk.Tk()
     master1.geometry("400x200")
-    master1.title("League Bot")
-    tk.Label(master1, text="Precentage").grid(row=0)
-    tk.Label(master1, text="Key").grid(row=1)
-    tk.Label(master1, text="Cooldown").grid(row=2)
-    tk.Label(master1, text="Dynamic health address").grid(row=3)
-    tk.Label(master1, text="Static health address").grid(row=4)
+    master1.title("Strong-Menu")
+    tk.Label(master1, 
+         text="           Game, as example:  League of Legends.exe").grid(row=0)
+    tk.Label(master1, text="Precentage").grid(row=1)
+    tk.Label(master1, text="Key, as example: ctrl+shift+f or f").grid(row=2)
+    tk.Label(master1, text="Cooldown of the spell").grid(row=3)
+    tk.Label(master1, text="Dynamic health/Mana address").grid(row=4)
+    tk.Label(master1, text="Static health/Mana address").grid(row=5)
 
-    
+    f = tk.Entry(master1)
     f1 = tk.Entry(master1)
     f2 = tk.Entry(master1)
     f3 = tk.Entry(master1)
     f4 = tk.Entry(master1)
     f5 = tk.Entry(master1)
-    
-    f1.grid(row=0, column=1)
-    f2.grid(row=1, column=1)
-    f3.grid(row=2, column=1)
-    f4.grid(row=3, column=1)
-    f5.grid(row= 4, column=1)
+    f.grid(row=0, column=1)
+    f1.grid(row=1, column=1)
+    f2.grid(row=2, column=1)
+    f3.grid(row=3, column=1)
+    f4.grid(row=4, column=1)
+    f5.grid(row= 5, column=1)
     def hell():
-        pm = Pymem("League of Legends.exe")
+        Game = str(f.get())
+        pm = Pymem(Game)
         Precentage = int(f1.get())
         print(Precentage)
         Key = f2.get().lower()
@@ -53,9 +58,12 @@ def new_window():
                     time.sleep(Cooldown)
             except:
                 continue
+    
+    def quit2():
+        master1.destroy(), new_window2()
     tk.Button(master1, text='Quit', command=master1.quit).grid(row=6, column=0, sticky=tk.W, pady=4)
     tk.Button(master1, text='Take the input', command=hell).grid(row=6, column=1, sticky=tk.W, pady=4)
-    tk.Button(master1, text="Health", command=new_window2).grid(row=6, column=2, sticky=tk.W, pady=4)
+    tk.Button(master1, text="Health", command=quit2).grid(row=6, column=2, sticky=tk.W, pady=4)
     tk.mainloop()
 
 
@@ -64,22 +72,27 @@ def new_window():
 def new_window2():
     master = tk.Tk()
     master.geometry("400x200")
-    master.title("League Bot")
-    tk.Label(master, text="Health").grid(row=0)
-    tk.Label(master, text="Key").grid(row=1)
-    tk.Label(master, text="Cooldown").grid(row=2)
-    tk.Label(master, text="Dynamic health address").grid(row=3)
+    master.title("Strong-Menu")
+    tk.Label(master, 
+         text="           Game, as example:  League of Legends.exe").grid(row=0)
+    tk.Label(master, text="Health/Mana").grid(row=1)
+    tk.Label(master, text="Key, as example: ctrl+shift+f or f").grid(row=2)
+    tk.Label(master, text="Cooldown of the spell").grid(row=3)
+    tk.Label(master, text="Dynamic health/Mana address").grid(row=4)
+    m = tk.Entry(master)
     m1 = tk.Entry(master)
     m2 = tk.Entry(master)
     m3 = tk.Entry(master)
     m4 = tk.Entry(master)
-    m1.grid(row=0, column=1)
-    m2.grid(row=1, column=1)
-    m3.grid(row=2, column=1)
-    m4.grid(row=3, column=1)
+    m.grid(row=0, column=1)
+    m1.grid(row=1, column=1)
+    m2.grid(row=2, column=1)
+    m3.grid(row=3, column=1)
+    m4.grid(row=4, column=1)
 
     def hell2():
-        pm = Pymem("League of Legends.exe")
+        Game = str(m.get())
+        pm = Pymem(Game)
         Health = int(m1.get())
         print(Health)
         Key = m2.get().lower()
@@ -97,19 +110,23 @@ def new_window2():
                     time.sleep(Cooldown)
             except:
                 continue
+    def quit1():
+        master.destroy(), new_window()
 
 
 
     tk.Button(master, text='Quit', command=master.quit).grid(row=5, column=0, sticky=tk.W, pady=4)
     tk.Button(master, text='Take the input', command=hell2).grid(row=5, column=1, sticky=tk.W, pady=4)
-    tk.Button(master, text="Precentage", command=new_window).grid(row=5, column=2, sticky=tk.W, pady=4)
+    tk.Button(master, text="Precentage", command=quit1).grid(row=5, column=2, sticky=tk.W, pady=4)
     tk.mainloop()
 
 
 
 def show_entry_fields():
-    pm = Pymem("League of Legends.exe")
+    Game = str(e.get())
+    pm = Pymem(Game)
 
+    
     Health = int(e1.get())
     print(Health)
     Key = e2.get().lower()
@@ -131,27 +148,33 @@ def show_entry_fields():
 
 
 master = tk.Tk()
-master.geometry("400x200")
-master.title("League Bot")
-
+master.geometry("700x200")
+master.title("Strong-Menu")
 
 
 tk.Label(master, 
-         text="Health").grid(row=0)
+         text="           Game, as example:  League of Legends.exe").grid(row=0)
 tk.Label(master, 
-         text="Key").grid(row=1)
+         text="Health/Mana").grid(row=1)
 tk.Label(master, 
-         text="Cooldown").grid(row=2)
-tk.Label(master, text="Dynamic health address").grid(row=3)
+         text="Key, as example: ctrl+shift+f or f").grid(row=2)
+tk.Label(master, 
+         text="Cooldown of the spell").grid(row=3)
+tk.Label(master, text="Dynamic Health/Mana address").grid(row=4)
 
+def quit():
+    master.destroy(), new_window()  
+
+e = tk.Entry(master)
 e1 = tk.Entry(master)
 e2 = tk.Entry(master)
 e3 = tk.Entry(master)
 e4 = tk.Entry(master)
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
-e3.grid(row=2, column=1)
-e4.grid(row=3, column=1)
+e.grid(row=0, column=2)
+e1.grid(row=1, column=2)
+e2.grid(row=2, column=2)
+e3.grid(row=3, column=2)
+e4.grid(row=4, column=2)
 tk.Button(master, 
           text='Quit', 
           command=master.quit).grid(row=5, 
@@ -159,5 +182,10 @@ tk.Button(master,
                                     sticky=tk.W, 
                                     pady=4)
 tk.Button(master, text='Take the input', command=show_entry_fields).grid(row=5, column=1, sticky=tk.W, pady=4)
-tk.Button(master, text="Precentage", command=new_window).grid(row=5, column=2, sticky=tk.W, pady=4)
+tk.Button(master, text="Precentage", command=quit).grid(row=5, column=2, sticky=tk.W, pady=4)
 tk.mainloop()
+
+
+
+
+
